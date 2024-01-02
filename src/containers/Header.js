57fd00f375920a181546d806b1fdb663253acc18
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-01-02 23:57:57 +0300

    feat: create structure of the redux app

---
+++ src/containers/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <d>
+      <h1>s</h1>
+      div
+    </d>
+  );
+};
+
+export default Header;
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { createStore } from "@reduxjs/toolkit";
+import reducers from "./reducers/index";
+const store = createStore(reducers, {});
+
+export default store;
